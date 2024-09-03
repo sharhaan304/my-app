878109142925082bfc1f7e98857944cd4c0a0065
sharhaan304 <sharhaanmfm.21@uom.lk>
2024-09-03 15:46:42 +0530

    debugging

---
+++ data/index.ts
@@ -0,0 +1,206 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Education", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for web development and LLM modeling.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a Gen-AI product",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: " Real time Chatbot By using RAG",
+      des: "Identify the customers query and deliver best solution by using RAG,LLM.",
+      img: "chat.avif",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+      link: "https://github.com/Hevb890/IFS_Project",
+    },
+    {
+      id: 2,
+      title: "Website for Supply chain Management",
+      des: "A website for managing the supply chain of a company from their owm data pipeline.",
+      img: "suply.jpg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      link: "https://github.com/Hevb890/IFS_Project",
+    },
+    {
+      id: 3,
+      title: "Python Sentiment Analysis",
+      des: "by using NLTK and BERT to rating the reviews of luxury Hotels.",
+      img: "4sen.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+      link: "https://github.com/sharhaan304/Python-Sentiment-anlaysis-using-NLTK-and-BERT",
+    },
+    {
+      id: 4,
+      title: "Clone a Social media App",
+      des: "recreate a social Media app by using Next.js and Tailwind CSS.",  
+      img: "social-media.jpg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://github.com/sharhaan304/Social",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "I completed my GCE Advanced Level (A/L) at T/R.K.M Hindu College, an institution known for its commitment to academic excellence and cultural values. During my time there, I specialized in subjects that challenged and expanded my intellectual boundaries, preparing me for the next steps in my academic and professional journey. The rigorous curriculum and supportive learning environment at T/R.K.M Hindu College played a crucial role in honing my analytical skills and deepening my knowledge in key subject areas. My experience at the college has been a cornerstone in shaping my career aspirations and personal growth, and I am proud to carry forward the lessons and principles I learned there..",
+      name: "T/R.K.M Sri Koneswara Hindu College",
+      title: "G.C.E A/L -> Grade 12-13",
+    },
+    {
+      quote:
+        "I am currently pursuing a Bachelor's degree in Artificial Intelligence at the University of Moratuwa, building upon a solid academic foundation from my time at T/R.K.M Hindu College, where I completed my GCE Advanced Level (A/L). My university studies are focused on the cutting-edge field of AI, where I am gaining deep expertise in machine learning, data science, and intelligent systems. The rigorous academic environment at the University of Moratuwa is enhancing my analytical and problem-solving skills, preparing me to contribute to the rapidly evolving tech industry. My journey through these esteemed institutions has been pivotal in shaping my academic interests and career goals, and I am eager to apply my knowledge in AI to real-world challenges.",
+      name: "University of Moratuwa",
+      title: "BSc(hons) in Artificial Intelligence -> Reading",
+    },
+    
+    
+    
+    
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Freelance Graphic Designer",
+      desc: "Creating banners, badges, Posters by using Adobe photoshop & Gimp.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: " Contract Mobile App Dev",
+      desc: "Designed and developed mobile app for both iOS & Android platforms using Flutter.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Freelance App Dev Project",
+      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Meme Creator",
+      desc: "Managing a page with More than 20K+ followers in Facebook .",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link: "https://github.com/sharhaan304"
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+      link:"https://x.com/sharhaanmfm"
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      link:"https://www.linkedin.com/in/mohamed-sharhaan-62b313247/ "
+    },
+  ];
