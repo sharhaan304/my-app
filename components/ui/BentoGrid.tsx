f8d512e5dd45cc47d483aeb6553b5ba1430bc504
sharhaan304 <sharhaanmfm.21@uom.lk>
2024-09-05 00:18:01 +0530

    bit changes

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -108,7 +108,7 @@ export const BentoGridItem = ({
                         <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
                             <div className="flex flex-col gap-3 lg:gap-5">
                                 <span className="py-3 px-2 rounded-lg text-center bg-[#10132e]"/>
-                                {['MERN','MEAN','GIT/GITHUB'].map((item) => (
+                                {['Python','JAVA','SQL/NoSQL'].map((item) => (
                                     <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
                                         {item}
                                     </span>
@@ -118,7 +118,7 @@ export const BentoGridItem = ({
                             </div>
                             <div className="flex flex-col gap-3 lg:gap-5">
                                 <span className="py-3 px-2 rounded-lg text-center bg-[#10132e]"/>
-                                {['Python','SQL/NoSQL','JAVA'].map((item) => (
+                                {['MEAN','MERN','Git/Github'].map((item) => (
                                     <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
                                         {item}
                                     </span>
